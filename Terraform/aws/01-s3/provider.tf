192a56161e3629f6c38664d26feed46680e38275
ShubhamNegi06 <shubh03uk06lko@outlook.com>
2026-07-19 09:00:26 +0530

    Initial Terraform AWS S3 lab using Floci

---
+++ Terraform/aws/01-s3/provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+
+  endpoints {
+    s3 = var.floci_endpoint
+  }
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id = true
+
+  s3_use_path_style = true
+}
+
+
---
+++ Terraform/aws/01-s3/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "access_key" {
+  default = "test"
+}
+
+variable "secret_key" {
+  default = "test"
+}
+
+variable "floci_endpoint" {
+  default = "http://192.168.1.100:4566"
+}
+
+variable "bucket_name" {
+  default = "terraform-lab-bucket-terraform"
+}
