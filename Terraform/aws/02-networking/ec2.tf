76adf44e74ffd837a33b43a5100a71ce83290fd5
ShubhamNegi06 <shubh03uk06lko@outlook.com>
2026-07-21 08:51:05 +0530

    Update ec2.tf

--- Terraform/aws/02-networking/ec2.tf
+++ Terraform/aws/02-networking/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami             = "ami-0abcdef1234567891"
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.public.id
-  security_groups = [aws_security_group.web-sg.id]
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   tags = {
     Name        = "terraform-web-server"
